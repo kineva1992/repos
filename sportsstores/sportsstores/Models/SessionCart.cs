631dbf66c7b918f7c2f4db9a0bd61723bd52a491
alexandex <kineva1992@gmail.com>
2021-02-14 14:18:44 +0300

    Create bar shop cart non action

--- sportsstores/sportsstores/Models/SessionCart.cs
+++ sportsstores/sportsstores/Models/SessionCart.cs
@@ -6,29 +6,39 @@ using sportsstores.Infrastructure;
 
 namespace sportsstores.Models
 {
+
     public class SessionCart : Cart
     {
-        public static Cart GetCart(IServiceProvider serviceProvider) {
-            ISession session = serviceProvider.GetRequiredService<IHttpContextAccessor>()?
+
+        public static Cart GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?
                 .HttpContext.Session;
             SessionCart cart = session?.GetJson<SessionCart>("Cart")
                 ?? new SessionCart();
+            cart.Session = session;
             return cart;
         }
+
         [JsonIgnore]
         public ISession Session { get; set; }
-        public override void AddItem(Product product, int quality) {
-            base.AddItem(product, quality);
+
+        public override void AddItem(Product product, int quantity)
+        {
+            base.AddItem(product, quantity);
             Session.SetJson("Cart", this);
         }
-        public override void RemoveLine(Product product) {
+
+        public override void RemoveLine(Product product)
+        {
             base.RemoveLine(product);
             Session.SetJson("Cart", this);
         }
-        public override void Clear() { 
+
+        public override void Clear()
+        {
             base.Clear();
             Session.Remove("Cart");
         }
-
     }
 }
