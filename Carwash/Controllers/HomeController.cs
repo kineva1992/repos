72e881cd67a479ef5a6bfa9360f2de67f4452c4c
alexandex <kineva1992@gmail.com>
2021-02-26 20:27:19 +0300

    Project CarWash

---
+++ Carwash/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Carwash.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //GET index page
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
